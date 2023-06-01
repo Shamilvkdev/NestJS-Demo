48f17b9ad775a35c6c15fe2aa644530916eae7df
Shamil <shamilvkage@gmail.com>
2023-06-01 15:16:14 +0530

    first day

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable({})
 export class AuthService {
-    login() {}
+    signup() {
+        return { msg: 'Signup successful' }
+    }
 
-    signup() {}
+    signin() {
+        return { msg: 'Signin successful' }
+    }
 }
