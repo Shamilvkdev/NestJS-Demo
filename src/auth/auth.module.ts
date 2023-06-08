f22d7bac622266a4af370440177f071f1265e877
Shamil <shamilvkage@gmail.com>
2023-06-08 14:15:42 +0530

    new changes.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { BookmarkModule } from "src/bookmark/bookmark.module";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+        }),
+        MongooseModule.forRoot(process.env.DB_URI),
+        BookmarkModule
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
---
+++ src/bookmark/schemas/book.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export enum Category {
+    ADVENTURE = 'Adventure',
+    CLASSICS = 'Classics',
+    CRIME = 'Crime',
+    FANTASY = ' Fantasy'
+}
+
+@Schema({
+    timestamps: true
+})
+export class Bookmark {
+
+    @Prop()
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    author: string;
+
+    @Prop()
+    price: number;
+
+    @Prop()
+    category: Category;
+}
+
+export const BookmarkSchema = SchemaFactory.createForClass(Bookmark)
