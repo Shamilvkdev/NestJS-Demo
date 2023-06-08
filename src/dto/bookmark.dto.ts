046d112fe78985d51207cfcbecd7df4036e98863
Shamil <shamilvkage@gmail.com>
2023-06-08 15:10:08 +0530

    new dto.

--- src/dto/bookmark.dto.ts
+++ src/dto/bookmark.dto.ts
@@ -3,6 +3,7 @@ import { Category } from "src/bookmark/schemas/bookmark.schema";
 export class CreateBookmarkDTO {
     readonly title: string;
     readonly description: string;
+    readonly author: string
     readonly price: number;
     readonly category: Category;
 }
