f982e2dfade6f52cf4931285cf1baa5f4d40afa8
Shamil <shamilvkage@gmail.com>
2023-06-13 12:39:31 +0530

    adding validators in update

--- src/dto/updateDTO.ts
+++ src/dto/updateDTO.ts
@@ -1,9 +1,23 @@
+import { IsEnum, IsOptional } from "class-validator";
 import { Category } from "src/bookmark/schemas/bookmark.schema";
 
 export class UpdateBookmarkDTO {
+
+    @IsOptional()
     readonly title: string;
+    
+    @IsOptional()
     readonly description: string;
+   
+    @IsOptional()
     readonly author: string
+   
+    @IsOptional()
     readonly price: number;
+   
+    @IsOptional()
+    @IsEnum(Category, {
+        message : 'Please enter a valid Category'
+    })
     readonly category: Category;
 }
