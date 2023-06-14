ec149a534ee04916e286831d36d3078300c8873a
Shamil <shamilvkage@gmail.com>
2023-06-14 14:54:56 +0530

    new schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,17 +2,28 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { BookmarkModule } from "src/bookmark/bookmark.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { config } from "process";
+import { UserSchema } from "./schema/user.schema";
 
 @Module({
     imports: [
-        ConfigModule.forRoot({
-            envFilePath: '.env',
-            isGlobal: true,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    secret: config.get<string>('JWT_SECRET'),
+                    signOptions: {
+                        expiresIn: config.get<string | number>('JWT_EXPIRES'),
+                    },
+                };
+            },
         }),
-        MongooseModule.forRoot(process.env.DB_URI),
-        BookmarkModule
+        MongooseModule.forFeature([{ name:'User', schema: UserSchema }]),
     ],
     controllers: [AuthController],
     providers: [AuthService],
