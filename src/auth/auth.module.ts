484040866bae7104d3b2153e24cd971bb880c2d4
Shamil <shamilvkage@gmail.com>
2023-07-02 12:18:23 +0530

    auth update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-// import { JwtStrategy } from './jwt.strategy';
 import { UserSchema } from './schema/user.schema';
 
 @Module({
