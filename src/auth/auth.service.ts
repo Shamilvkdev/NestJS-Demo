946413284d9798a5f8963867732fd9249033bd33
Shamil <shamilvkage@gmail.com>
2023-08-03 11:22:57 +0530

    structure update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./schema/user.schema";
 import { Model } from "mongoose";
-
 import * as bcrypt from "bcryptjs"
 import { JwtService } from "@nestjs/jwt";
 import { SignUpDto } from "./dto/signup.dto";
